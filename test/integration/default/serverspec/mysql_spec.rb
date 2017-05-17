f47dd7a7de20f7bda60826e4f984ac2906e8b01c
Andres Montalban <amontalban@devopx.com>
2017-05-17 11:59:41 -0300

    [ADD] Add serverspec tests for MySQL

---
+++ test/integration/default/serverspec/mysql_spec.rb
@@ -0,0 +1,87 @@
+require 'serverspec'
+require 'spec_helper'
+
+# Required by serverspec
+set :backend, :exec
+
+mysql = YAML.load_file('/tmp/mysql.config')
+mysql_query = "mysql --defaults-extra-file=/root/.my.cnf mysql -sN -e"
+
+describe "MySQL" do
+  # ===== MySQL critical tests ======
+  it "server package is installed" do
+    expect(package(mysql['server'])).to be_installed
+  end
+
+  it "client package is installed" do
+    expect(package(mysql['client'])).to be_installed
+  end
+
+  it "is listening on port #{mysql['config']['sections']['mysqld']['port'] || 3306}" do
+    expect(port(mysql['config']['sections']['mysqld']['port'] || 3306)).to be_listening
+  end
+
+  it "service is running" do
+    expect(service(mysql['service'])).to be_running
+  end
+
+  it "is enabled to start on boot" do
+    expect(service(mysql['service'])).to be_enabled
+  end
+
+  # ===== MySQL critical settings ======
+  it "timezone is #{mysql['config']['sections']['mysqld']['default_time_zone']}" do
+    expect(command("#{mysql_query} \"show variables like 'time_zone';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['default_time_zone']}/)
+  end
+
+  # ===== MySQL Log files ======
+  it "log_error is configured to #{mysql['config']['sections']['mysqld']['log_error']}" do
+    expect(command("#{mysql_query} \"show variables like 'log_error';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['log_error']}/)
+  end
+
+  it "#{mysql['config']['sections']['mysqld']['log_error']} is a file" do
+    expect(file("#{mysql['config']['sections']['mysqld']['log_error']}")).to be_file
+  end
+
+  it "#{mysql['config']['sections']['mysqld']['log_error']} is a owned by #{mysql['config']['sections']['mysqld']['user']}" do
+    expect(file("#{mysql['config']['sections']['mysqld']['log_error']}")).to be_owned_by mysql['config']['sections']['mysqld']['user']
+  end
+
+  it "slow_query_log_file is configured to #{mysql['config']['sections']['mysqld']['slow_query_log_file']}" do
+    expect(command("#{mysql_query} \"show variables like 'slow_query_log_file';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['slow_query_log_file']}/)
+  end
+
+  it "#{mysql['config']['sections']['mysqld']['slow_query_log_file']} is a file" do
+    expect(file("#{mysql['config']['sections']['mysqld']['slow_query_log_file']}")).to be_file
+  end
+
+  it "#{mysql['config']['sections']['mysqld']['slow_query_log_file']} is a owned by #{mysql['config']['sections']['mysqld']['user']}" do
+    expect(file("#{mysql['config']['sections']['mysqld']['slow_query_log_file']}")).to be_owned_by mysql['config']['sections']['mysqld']['user']
+  end
+
+  # ===== MySQL other settings ======
+  it "datadir is #{mysql['config']['sections']['mysqld']['datadir']}" do
+    expect(command("#{mysql_query} \"show variables like 'datadir';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['datadir']}/)
+  end
+
+  it "binlog_format is #{mysql['config']['sections']['mysqld']['binlog_format']}" do
+    expect(command("#{mysql_query} \"show variables like 'binlog_format';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['binlog_format']}/)
+  end
+
+  it "max_connections is #{mysql['config']['sections']['mysqld']['max_connections']}" do
+    expect(command("#{mysql_query} \"show variables like 'max_connections';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['max_connections']}/)
+  end
+
+  it "innodb_file_format is #{mysql['config']['sections']['mysqld']['innodb_file_format']}" do
+    expect(command("#{mysql_query} \"show variables like 'innodb_file_format';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['innodb_file_format']}/)
+  end
+
+  it "character_set_server is #{mysql['config']['sections']['mysqld']['character_set_server']}" do
+    expect(command("#{mysql_query} \"show variables like 'character_set_server';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['character_set_server']}/)
+  end
+
+  it "default_storage_engine is #{mysql['config']['sections']['mysqld']['default_storage_engine']}" do
+    expect(command("#{mysql_query} \"show variables like 'default_storage_engine';\"").stdout).to match(/#{mysql['config']['sections']['mysqld']['default_storage_engine']}/)
+  end
+
+end
---
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'serverspec'
+require 'yaml'
+
+set :backend, :exec
+
+system "salt-call --local --config-dir=/tmp/kitchen/etc/salt -l quiet cp.get_template salt://mysql/files/rawmap.yml /tmp/mysql.config 2>&1 > /dev/null"
+
+# Configure OS specific parameters
+if ['debian', 'ubuntu'].include?(os[:family])
+  unless File.exists?('/bin/netstat')
+    system "apt-get install -y net-tools"
+  end
+else
+  # Install netstat that is needed to check port is listening
+end
